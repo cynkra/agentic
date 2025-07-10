7a1de258269f187dd7cf853a3018abf6cd7c58ce
Antoine Fabri <antoine.fabri@gmail.com>
2025-07-10 19:48:28 +0200

    export agent and fix printing

--- R/agent.R
+++ R/agent.R
@@ -1,9 +1,22 @@
-agent <- function(model_family = "openai", ...) {
+#' Create an agent with all agentic tools
+#'
+#' This function creates an agent using the specified model family (default: 'openai'),
+#' registers all available agentic tools, and returns the agent object.
+#'
+#' @param provider The provider to use.
+#' @param ... Additional arguments passed to the chat function for the provider.
+#' @return An Chat object with all agentic tools registered.
+#' @examples
+#' ag <- agent()
+#' ag$chat("What time is it?")
+#' @export
+agent <- function(provider = "openai", ...) {
     chat_fun_nm <- paste0("chat_", model_family)
     chat_fun <- getFromNamespace(chat_fun_nm, "ellmer")
-    ag <- chat_fun(...)
     agentic_ns <- asNamespace("agentic")
-    tools <- ls(agentic_ns, pattern= "^tool_")
+    tools <- ls(agentic_ns, pattern = "^tool_")
+
+    ag <- do.call(chat_fun, list(...), envir = globalenv())
     for (tool in tools) {
       ag$register_tool(agentic_ns[[tool]]())
     }
